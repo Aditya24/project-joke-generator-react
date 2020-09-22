1be3696381b0fd4e405767c3aadffca12b97c512
Aditya24 <aadityabante@gmail.com>
2020-09-22 23:14:30 +0200

    fetching the data on UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Joke from "./Joke";
 import "./App.css";
 
 function App() {
-  return <Joke />;
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    const url =
+      "https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=religious&type=single";
+    const fetchJokes = async function () {
+      const response = await fetch(url);
+      const joke = await response.json();
+      console.log(joke);
+      setJokes(joke);
+    };
+    fetchJokes();
+  }, []);
+
+  return <Joke jokes={jokes} />;
 }
 
 export default App;
--- src/Joke.js
+++ src/Joke.js
@@ -1,15 +1,15 @@
 import React from "react";
 import "./App.css";
 
-const Joke = () => {
+const Joke = ({ jokes }) => {
   return (
     <>
       <h1 className='main-heading'>One joke a day keeps the bugs away! </h1>
       <div className='joke-container'>
         <div className='joke-item'>
-          <h2 className='joke-heading content'>Joke of the day</h2>
-          <p className='joke-category content'>Category:</p>
-          <p className='joke content'>on Angular I don't know how to React</p>
+          <h2 className='joke-heading content'></h2>
+          <p className='joke-category content'>Category:{jokes.category}</p>
+          <p className='joke content'>{jokes.joke}</p>
         </div>
       </div>
     </>
