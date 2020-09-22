82499bfaed3ff3f6ef37b8f2cabafc5b8b43a6a3
Aditya24 <aadityabante@gmail.com>
2020-09-23 01:03:45 +0200

    Updates

--- precache-manifest.b724b19684e962bd2a890e5c3591fc23.js
+++ precache-manifest.b724b19684e962bd2a890e5c3591fc23.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e3b8fd5eef291a17b3cf1831fb3d7b59",
+    "revision": "d25a03f7183a71a3ba38d68f719a2dc0",
     "url": "/project-joke-generator-react/index.html"
   },
   {
-    "revision": "4a95a38bed489e1175ef",
-    "url": "/project-joke-generator-react/static/css/main.57f6a944.chunk.css"
+    "revision": "20045b528474cfd8ed62",
+    "url": "/project-joke-generator-react/static/css/main.b2be6f45.chunk.css"
   },
   {
     "revision": "ec4a449489a3d5d062bd",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/project-joke-generator-react/static/js/2.0c9aefb6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4a95a38bed489e1175ef",
-    "url": "/project-joke-generator-react/static/js/main.44b9453a.chunk.js"
+    "revision": "20045b528474cfd8ed62",
+    "url": "/project-joke-generator-react/static/js/main.e3460a25.chunk.js"
   },
   {
     "revision": "d60d5ce3dd57564153d7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-joke-generator-react/precache-manifest.a6febd7cf9dc5d220b499e5878b248bf.js"
+  "/project-joke-generator-react/precache-manifest.b724b19684e962bd2a890e5c3591fc23.js"
 );
 
 self.addEventListener('message', (event) => {
