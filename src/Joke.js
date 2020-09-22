5b47dcc66fde0c32c8e52bba9d70e879b98cb59d
Aditya24 <aadityabante@gmail.com>
2020-09-23 00:51:17 +0200

    styling

--- src/Joke.js
+++ src/Joke.js
@@ -5,7 +5,7 @@ const Joke = ({ jokes }) => {
   return (
     <>
       <h1 className='main-heading'>One joke a day keeps the bugs away! </h1>
-      <h2 className='bottom-heading'>Refresh for more... </h2>
+
       <div className='joke-container'>
         <div className='joke-item'>
           <h2 className='joke-heading content'></h2>
@@ -16,6 +16,7 @@ const Joke = ({ jokes }) => {
           </p>
         </div>
       </div>
+      <h2 className='bottom-heading'>Refresh for more... </h2>
     </>
   );
 };
