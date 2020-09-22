0305291e3bb9d8e00133a28a00e15835def9ff64
Aditya24 <aadityabante@gmail.com>
2020-09-23 01:04:12 +0200

    minor

--- src/Joke.js
+++ src/Joke.js
@@ -5,16 +5,15 @@ const Joke = ({ jokes }) => {
   return (
     <>
       <h1 className='main-heading'>One joke a day keeps the bugs away! </h1>
-
+      <h3 className='bottom-heading'>Refresh for more... </h3>
       <div className='joke-container'>
         <div className='joke-item'>
-          <p className='joke content'>
+          <p className='joke'>
             {quote}
             {jokes.joke}
           </p>
         </div>
       </div>
-      <h2 className='bottom-heading'>Refresh for more... </h2>
     </>
   );
 };
