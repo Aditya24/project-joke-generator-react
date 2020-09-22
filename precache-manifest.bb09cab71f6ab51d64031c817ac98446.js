7d0972d94cace123780f7ac69e04063bc4af7620
Aditya24 <aadityabante@gmail.com>
2020-09-23 00:40:56 +0200

    Updates

---
+++ precache-manifest.bb09cab71f6ab51d64031c817ac98446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4172bf73119d4e5b217127fcdd06e8f0",
+    "url": "/project-joke-generator-react/index.html"
+  },
+  {
+    "revision": "599f80c638d335180a33",
+    "url": "/project-joke-generator-react/static/css/main.caa2643e.chunk.css"
+  },
+  {
+    "revision": "ec4a449489a3d5d062bd",
+    "url": "/project-joke-generator-react/static/js/2.0c9aefb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-joke-generator-react/static/js/2.0c9aefb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599f80c638d335180a33",
+    "url": "/project-joke-generator-react/static/js/main.c5de9362.chunk.js"
+  },
+  {
+    "revision": "d60d5ce3dd57564153d7",
+    "url": "/project-joke-generator-react/static/js/runtime-main.bd189e48.js"
+  }
+]);
